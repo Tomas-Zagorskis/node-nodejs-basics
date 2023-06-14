8d3210f3bca4ad283de9887d78262647e69806f3
Tomas_Zagorskis <tomzagos@gmail.com>
2023-06-14 23:06:30 +0300

    feat: implement fs delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.resolve(__dirname, 'files/fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+	fs.rm(filePath, err => {
+		if (err) throw new Error('FS operation failed');
+	});
 };
 
 await remove();
