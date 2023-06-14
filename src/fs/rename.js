df41de237a7b0c6a9e92d053492fb00f5e479a40
Tomas_Zagorskis <tomzagos@gmail.com>
2023-06-14 23:02:37 +0300

    feat: implement fs rename file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const oldPath = path.resolve(__dirname, 'files/wrongFilename.txt');
+const newPath = path.resolve(__dirname, 'files/properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+	fs.access(newPath, err => {
+		if (!err) throw new Error('FS operation failed');
+
+		fs.rename(oldPath, newPath, err => {
+			if (err) throw new Error('FS operation failed');
+		});
+	});
 };
 
 await rename();
