2263db6d032ad52fc22e63f0211b80249bd66a97
Tomas_Zagorskis <tomzagos@gmail.com>
2023-06-15 22:55:16 +0300

    feat: implement reverse text transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform, pipeline } from 'stream';
+
+const input = process.stdin;
+const output = process.stdout;
+
+const reverseStr = new Transform({
+	transform(chunk, _encoding, callback) {
+		this.push(`${chunk.toString().trim().split('').reverse().join('')}\n`);
+		callback();
+	},
+});
+
 const transform = async () => {
-    // Write your code here 
+	pipeline(input, reverseStr, output, err => {
+		if (err) throw new Error('Pipeline failed');
+	});
 };
 
 await transform();
