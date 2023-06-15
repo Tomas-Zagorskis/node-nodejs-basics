3af9452827d9316907095ac2d1ec42e705743bd3
Tomas_Zagorskis <tomzagos@gmail.com>
2023-06-15 20:41:30 +0300

    feat: implement cli argv

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
+import { argv } from 'process';
+
+// execute in terminal: node src/cli/args --propName 123 --prop2Name 'Hello'
+
 const parseArgs = () => {
-    // Write your code here 
+	const result = argv
+		.filter(arg => arg.match(/--\w*/))
+		.map(value => `${value.slice(2)} is ${argv[argv.indexOf(value) + 1]}`)
+		.join(', ');
+	console.log(result);
 };
 
 parseArgs();
