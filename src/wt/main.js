fc47ef2442252f24a9c1f22523b15e3511858560
Tomas Zagorskis <tomzagos@gmail.com>
2023-06-18 22:44:51 +0300

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { Worker } from 'worker_threads';
+import * as os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const workerPath = path.resolve(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+	const result = [];
+	const numOfCPUCores = os.cpus().length;
+
+	for (let i = 10; i < numOfCPUCores + 10; i++) {
+		let worker = new Worker(workerPath, { workerData: i });
+		result.push(
+			new Promise(resolve => {
+				worker.on('message', data => {
+					resolve(data);
+				});
+				worker.on('error', () => {
+					resolve({ status: 'error', data: null });
+				});
+			}),
+		);
+	}
+
+	console.log(await Promise.all(result));
 };
 
 await performCalculations();
